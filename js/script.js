35a286f52400ca402bd4a7c685ce2ef10a416144
Lavanya <lavanyavasanth1988@gmail.com>
2019-03-19 19:12:20 +1000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    //toggle btn
+    $('.navbar-toggler').click(function(){
+       $('.navbar-toggler').toggleClass('change') 
+    });
+    //sticky navbar less padding
+    $(window).scroll(function(){
+       let position = $(this).scrollTop();
+        if(position>= 660){
+            $('.navbar').addClass('navbar-background');
+            $('.navbar').addClass('fixed-top');
+        } else {
+            $('.navbar').removeClass('navbar-background');
+            $('.navbar').removeClass('fixed-top');
+        }
+    })
+    //smoothscroll
+    $('.nav-item a,.header-link,#back-to-top').click(function(link){
+        link.preventDefault();
+        let target = $(this).attr('href');
+        $('html,body').stop().animate({
+            scrollTop: $(target).offset().top - 25
+        },3000);
+    })
+    //back to top
+       $(window).scroll(function(){
+       let position = $(this).scrollTop();
+        if(position>= 660){
+            $('#back-to-top').addClass('scrollTop');
+        } else {
+            $('#back-to-top').removeClass('scrollTop');
+        }
+    })
+    //ripples
+    $(".info").ripples({
+    dropRadius: 25,
+    perturbance: 0.6
+    });
+    //magnific popup
+    $('.parent-container').magnificPopup({
+      delegate: 'a', // child items selector, by clicking on it popup will open
+      type: 'image',
+      gallery:{
+          enabled:true
+      }
+      // other options
+    });
+});
